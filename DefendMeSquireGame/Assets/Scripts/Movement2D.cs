111e69b51e3a2a2fb5befdbae311e03e0cf1b1e0
Ben Partridge <ben.partridge249@gmail.com>
2020-03-17 23:22:36 +1100

    Create basic movement script and demo scene with hero 1 sprite

---
+++ DefendMeSquireGame/Assets/Scripts/Movement2D.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement2D : MonoBehaviour
+{
+    public float speed = 3f;
+
+    private Rigidbody2D rb2d;
+
+    void Start() {
+        rb2d = GetComponent<Rigidbody2D>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float currentVertVelocity = rb2d.velocity.y;
+        if (currentVertVelocity == 0) {
+            float horizontalVelocity = Input.GetAxis("Horizontal") * speed;
+            rb2d.velocity = new Vector2(horizontalVelocity, currentVertVelocity);
+        } else {
+            rb2d.velocity = new Vector2(0, currentVertVelocity);
+        }
+
+    }
+}

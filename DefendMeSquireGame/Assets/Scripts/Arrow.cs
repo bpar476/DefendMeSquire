b6a94510b4ad703fa3146152f72921f6aa6c20fe
Ben Partridge <ben.partridge249@gmail.com>
2020-03-26 20:38:08 +1100

    cache velocity of arrow and only update rotation when it has changed significantly

--- DefendMeSquireGame/Assets/Scripts/Arrow.cs
+++ DefendMeSquireGame/Assets/Scripts/Arrow.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Arrow : MonoBehaviour
 {
     private Rigidbody2D rb;
+    private Vector2 cachedVelocity;
+    private bool collided;
     
     void Start()
     {
@@ -13,7 +15,22 @@ public class Arrow : MonoBehaviour
 
     void Update()
     {
-        float angle = Vector2.SignedAngle(Vector2.right, rb.velocity);
-        transform.rotation = Quaternion.Euler(0, 0, angle);
+        Vector2 currentVelocity = rb.velocity;
+        if (velocityMeaningfullyChanged(currentVelocity)) {
+            float angle = Vector2.SignedAngle(Vector2.right, currentVelocity);
+            transform.rotation = Quaternion.Euler(0, 0, angle);
+        }
+    }
+
+    private bool velocityMeaningfullyChanged(Vector2 currentVelocity) {
+        bool xMeaningfullyChanged = floatsMeaningfullyDifferent(currentVelocity.x, cachedVelocity.x, 0.1f);
+        bool yMeaningfullyChanged = floatsMeaningfullyDifferent(currentVelocity.y, cachedVelocity.y, 0.1f);
+        return xMeaningfullyChanged || yMeaningfullyChanged;
+    }
+
+    private bool floatsMeaningfullyDifferent(float a, float b, float tolerance) {
+        float difference = a - b;
+        float absTolerance = Mathf.Abs(tolerance);
+        return difference > absTolerance || difference < 0 - absTolerance;
     }
 }

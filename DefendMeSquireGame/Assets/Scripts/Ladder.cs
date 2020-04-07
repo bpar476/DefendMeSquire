e6d4ec51d16ea290b2782df4764daeb69806cebc
Ben Partridge <ben.partridge249@gmail.com>
2020-04-07 19:55:58 +1000

    Move folders around in prototyping directory

---
+++ DefendMeSquireGame/Assets/Scripts/Ladder.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ladder : MonoBehaviour
+{
+
+    public float grabRadius;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnDrawGizmosSelected() {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position - new Vector3(grabRadius, 0, 0), transform.position + new Vector3(grabRadius, 0, 0));
+    }
+}

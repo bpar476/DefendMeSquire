c2a1baec60507cc513e867def32997cd90e64890
Ben Partridge <ben.partridge249@gmail.com>
2020-04-07 20:29:33 +1000

    Implement detecting if player is in ladder.

--- DefendMeSquireGame/Assets/Scripts/Ladder.cs
+++ DefendMeSquireGame/Assets/Scripts/Ladder.cs
@@ -5,21 +5,67 @@ using UnityEngine;
 public class Ladder : MonoBehaviour
 {
 
-    public float grabRadius;
+    public float grabRadiusX;
+    public float grabRadiusY;
+
+    private Vector3 left {
+        get {
+            return transform.position - new Vector3(grabRadiusX, 0, 0);
+        }
+    }
+
+    private Vector3 right {
+        get {
+            return transform.position + new Vector3(grabRadiusX, 0, 0);
+        }
+    }
+
+    private Vector3 bottom {
+        get {
+            return transform.position - new Vector3(0, grabRadiusY, 0);
+        }
+    }
+
+    private Vector3 top {
+        get {
+            return transform.position + new Vector3(0, grabRadiusY, 0);
+        }
+    }
+
+    private Vector3 topRight {
+        get {
+            return top + new Vector3(0, grabRadiusX, 0);
+        }
+    }
+
+    private Vector3 bottomLeft {
+        get {
+            return bottom - new Vector3(0, grabRadiusX, 0);
+        }
+    }
+
+    private SpriteRenderer spriteRenderer;
 
     void Start()
     {
-        
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+
+        Bounds bounds = new Bounds(transform.position, new Vector3(grabRadiusX * 2, grabRadiusY * 2, 0));
+        if (bounds.Contains(player.transform.position)) {
+
+        }
+
     }
 
     private void OnDrawGizmosSelected() {
         Gizmos.color = Color.red;
-        Gizmos.DrawLine(transform.position - new Vector3(grabRadius, 0, 0), transform.position + new Vector3(grabRadius, 0, 0));
+        Gizmos.DrawLine(left, right);
+        Gizmos.DrawLine(bottom, top);
     }
 }

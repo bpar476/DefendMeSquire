21a180db3e57fa57bc38e2dfbf0db2d3053ac48b
Ben Partridge <ben.partridge249@gmail.com>
2020-04-07 20:45:38 +1000

    Refactor ladder to use colliders

--- DefendMeSquireGame/Assets/Scripts/Ladder.cs
+++ DefendMeSquireGame/Assets/Scripts/Ladder.cs
@@ -4,68 +4,30 @@ using UnityEngine;
 
 public class Ladder : MonoBehaviour
 {
-
-    public float grabRadiusX;
-    public float grabRadiusY;
-
-    private Vector3 left {
-        get {
-            return transform.position - new Vector3(grabRadiusX, 0, 0);
-        }
-    }
-
-    private Vector3 right {
-        get {
-            return transform.position + new Vector3(grabRadiusX, 0, 0);
-        }
-    }
-
-    private Vector3 bottom {
-        get {
-            return transform.position - new Vector3(0, grabRadiusY, 0);
-        }
-    }
-
-    private Vector3 top {
-        get {
-            return transform.position + new Vector3(0, grabRadiusY, 0);
-        }
-    }
-
-    private Vector3 topRight {
-        get {
-            return top + new Vector3(0, grabRadiusX, 0);
-        }
-    }
-
-    private Vector3 bottomLeft {
-        get {
-            return bottom - new Vector3(0, grabRadiusX, 0);
-        }
-    }
-
-    private SpriteRenderer spriteRenderer;
-
-    void Start()
-    {
-        spriteRenderer = GetComponent<SpriteRenderer>();
-    }
+    public float climbSpeed;
 
     // Update is called once per frame
     void Update()
     {
         GameObject player = GameObject.FindGameObjectWithTag("Player");
+        Rigidbody2D rb2d = player.GetComponent<Rigidbody2D>();
 
-        Bounds bounds = new Bounds(transform.position, new Vector3(grabRadiusX * 2, grabRadiusY * 2, 0));
-        if (bounds.Contains(player.transform.position)) {
+        float verticalInput = Input.GetAxis("Vertical");
+        if (verticalInput != 0) {
 
+                rb2d.velocity = new Vector2(0, Mathf.Sign(verticalInput) * climbSpeed);
         }
-
     }
 
-    private void OnDrawGizmosSelected() {
-        Gizmos.color = Color.red;
-        Gizmos.DrawLine(left, right);
-        Gizmos.DrawLine(bottom, top);
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.tag.Equals("Player")) {
+            Debug.Log("player enters ladder");
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other) {
+        if (other.gameObject.tag.Equals("Player")) {
+            Debug.Log("player exits ladder");
+        }
     }
 }

315cd07b331d044b9317135f805d393491245e43
Ben Partridge <ben.partridge249@gmail.com>
2020-04-07 20:55:04 +1000

    Implement climbing using translation

--- DefendMeSquireGame/Assets/Scripts/Ladder.cs
+++ DefendMeSquireGame/Assets/Scripts/Ladder.cs
@@ -6,28 +6,45 @@ public class Ladder : MonoBehaviour
 {
     public float climbSpeed;
 
+    private GameObject player;
+    private bool playerInLadder;
+
+    private float actualClimbSpeed;
+
+    private void Start() {
+        actualClimbSpeed = climbSpeed / 60;
+    }
+
     // Update is called once per frame
     void Update()
     {
-        GameObject player = GameObject.FindGameObjectWithTag("Player");
-        Rigidbody2D rb2d = player.GetComponent<Rigidbody2D>();
-
         float verticalInput = Input.GetAxis("Vertical");
-        if (verticalInput != 0) {
-
-                rb2d.velocity = new Vector2(0, Mathf.Sign(verticalInput) * climbSpeed);
+        if (verticalInput != 0 && playerInLadder) {
+            player.transform.Translate(new Vector3(0, Mathf.Sign(verticalInput) * actualClimbSpeed), 0);
         }
     }
 
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.gameObject.tag.Equals("Player")) {
             Debug.Log("player enters ladder");
+            playerInLadder = true;
+
+            player = other.gameObject;
+
+            Rigidbody2D rb2d = player.GetComponent<Rigidbody2D>();
+            rb2d.gravityScale = 0;
         }
     }
 
     private void OnTriggerExit2D(Collider2D other) {
         if (other.gameObject.tag.Equals("Player")) {
             Debug.Log("player exits ladder");
+            playerInLadder = false;
+
+            player = other.gameObject;
+
+            Rigidbody2D rb2d = player.GetComponent<Rigidbody2D>();
+            rb2d.gravityScale = 1;
         }
     }
 }

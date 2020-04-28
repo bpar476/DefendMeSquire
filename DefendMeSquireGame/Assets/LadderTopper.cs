09a35705c902637efc88b5e3d52e6cd27b29f790
Ben Partridge <ben.partridge249@gmail.com>
2020-04-28 19:55:19 +1000

    Implement 1 way platform for ladders

---
+++ DefendMeSquireGame/Assets/LadderTopper.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LadderTopper : MonoBehaviour
+{
+    public Ladder ladder;
+
+    private Collider2D col;
+
+    private void Start() {
+        col = GetComponent<Collider2D>();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+       if (ladder.isPlayerOnLadder()) {
+           col.enabled = false;
+       } else {
+           col.enabled = true;
+       }
+    }
+}

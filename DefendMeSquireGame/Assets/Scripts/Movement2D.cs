165c89d52d77f78a20112ed977e643c82b7e998b
Ben Partridge <ben.partridge249@gmail.com>
2020-05-02 14:55:16 +1000

    Enforce no horizontal motion while falling

--- DefendMeSquireGame/Assets/Scripts/Movement2D.cs
+++ DefendMeSquireGame/Assets/Scripts/Movement2D.cs
@@ -20,22 +20,32 @@ public class Movement2D : MonoBehaviour
     {
         float currentDir = Mathf.Sign(rb2d.velocity.x);
 
-        if (Input.GetAxis("Horizontal") != 0)
-        {
-            stopped = false;
-
-            float inputDir = Mathf.Sign(Input.GetAxis("Horizontal"));
-
-            if (inputDir * rb2d.velocity.x < maxMoveSpeed)
-            {
-                rb2d.AddForce(new Vector2(inputDir * speed, 0), ForceMode2D.Impulse);
-            }
-
-            rb2d.velocity = new Vector2(Mathf.Clamp(rb2d.velocity.x, -maxMoveSpeed, maxMoveSpeed), rb2d.velocity.y);
-        }
-        else
+        if (IsFalling())
         {
             rb2d.velocity = new Vector2(0, rb2d.velocity.y);
+        } else {
+            if (Input.GetAxis("Horizontal") != 0)
+            {
+                stopped = false;
+
+                float inputDir = Mathf.Sign(Input.GetAxis("Horizontal"));
+
+                if (inputDir * rb2d.velocity.x < maxMoveSpeed)
+                {
+                    rb2d.AddForce(new Vector2(inputDir * speed, 0), ForceMode2D.Impulse);
+                }
+
+                rb2d.velocity = new Vector2(Mathf.Clamp(rb2d.velocity.x, -maxMoveSpeed, maxMoveSpeed), rb2d.velocity.y);
+            }
+            else
+            {
+                rb2d.velocity = new Vector2(0, rb2d.velocity.y);
+            }
         }
+    }
+
+    private bool IsFalling()
+    {
+        return rb2d.velocity.y < -0.01;
     }
 }

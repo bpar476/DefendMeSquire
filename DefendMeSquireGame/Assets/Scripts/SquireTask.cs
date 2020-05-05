a9da95d6e1a405f516285d8c8da97090d0913919
Ben Partridge <ben.partridge249@gmail.com>
2020-05-05 20:26:33 +1000

    Implement basic script for completing tasks

---
+++ DefendMeSquireGame/Assets/Scripts/SquireTask.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SquireTask : MonoBehaviour
+{
+
+    private LoadingBar loadingBar;
+
+    private bool isBeingCompleted = false;
+    private bool playerIsOnTask = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        loadingBar = GetComponentInChildren<LoadingBar>();
+        if (loadingBar == null) {
+            throw new System.Exception("loading bar is not on child component");
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (IsPlayer(other.gameObject)) {
+            playerIsOnTask = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other) {
+        if (IsPlayer(other.gameObject)) {
+            playerIsOnTask = false;
+        }
+    }
+
+    private bool IsPlayer(GameObject gobj) {
+            return gobj.tag == "Player";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool playerTryingToDotask = Input.GetAxis("Interaction") != 0;
+        if (playerTryingToDotask && playerIsOnTask) {
+            isBeingCompleted = true;
+            StartPerformingTask();
+        } else {
+            if (isBeingCompleted) {
+                StopPerformingTask();
+            }
+            isBeingCompleted = false;
+        }
+    }
+
+    void StartPerformingTask() {
+        Debug.Log("Player is doing task");
+        // Start loading bar
+    }
+
+    void StopPerformingTask() {
+        Debug.Log("Player stopped doing task");
+        // Stop loading bar
+    }
+
+}

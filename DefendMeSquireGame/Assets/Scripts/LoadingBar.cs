76f08e8ab010d4c9e776bf51037ae9cbb8f452b6
Ben Partridge <ben.partridge249@gmail.com>
2020-05-05 20:01:07 +1000

    Implement script to render a loading bar with a given duration

---
+++ DefendMeSquireGame/Assets/Scripts/LoadingBar.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadingBar : MonoBehaviour
+{
+    public float duration;
+
+    public List<Sprite> loadingSprites;
+    private bool finished = false;
+    private SpriteRenderer myRenderer;
+    private int currentIncrement = 0;
+    private float time = 0.0f;
+    private float totalTime = 0.0f;
+    private float incrementThreshold {
+        get {
+            return duration / increments;
+        }
+    }
+    private int increments {
+        get {
+            return loadingSprites.Count - 1;
+        }
+    }
+
+    void Start()
+    {
+        myRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update() {
+        if (finished) {
+            return;
+        }
+
+        time += Time.deltaTime;
+        totalTime += Time.deltaTime;
+
+        if (time >= incrementThreshold) {
+            currentIncrement++;
+            myRenderer.sprite = loadingSprites[currentIncrement];
+
+            if (currentIncrement == increments) {
+                finished = true;
+                Debug.Log(totalTime);
+            }
+
+            time -= incrementThreshold;
+        }
+    }
+}

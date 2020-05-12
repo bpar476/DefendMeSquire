d180c43fca24bbfc7ab729c77f452e198b88d895
Ben Partridge <ben.partridge249@gmail.com>
2020-05-12 17:55:07 +1000

    Implement hit points UI

--- DefendMeSquireGame/Assets/Scripts/Health.cs
+++ DefendMeSquireGame/Assets/Scripts/Health.cs
@@ -1,15 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Health : MonoBehaviour
 {
     public int maxHitPoints;
+    public Text hitpointsUI;
 
     private int currentHitPoints;
 
     private void Start() {
         currentHitPoints = maxHitPoints;
+        UpdateHitpointsUI();
     }
 
     private void OnCollisionEnter2D(Collision2D other) {
@@ -17,9 +20,23 @@ public class Health : MonoBehaviour
         if (damage != null) {
             currentHitPoints -= damage.hits;
 
+            UpdateHitpointsUI();
+
             if (currentHitPoints <= 0) {
                 Die();
             }
+        }
+    }
+
+    private void UpdateHitpointsUI() {
+        hitpointsUI.text = currentHitPoints.ToString();
+
+        if (currentHitPoints == maxHitPoints) {
+            hitpointsUI.color = Color.green;
+        } else if (currentHitPoints == 0) {
+            hitpointsUI.color = Color.red;
+        } else {
+            hitpointsUI.color = Color.yellow;
         }
     }
 

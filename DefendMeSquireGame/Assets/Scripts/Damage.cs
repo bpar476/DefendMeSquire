51d9b1c89d684936b54b7b5bc36d6c853f377f71
Ben Partridge <ben.partridge249@gmail.com>
2020-05-12 17:45:45 +1000

    Implement basic hit points and damage

---
+++ DefendMeSquireGame/Assets/Scripts/Damage.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Damage : MonoBehaviour
+{
+    /// How many hit points this damage object deals
+    public int hits;
+}
---
+++ DefendMeSquireGame/Assets/Scripts/Health.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public int maxHitPoints;
+
+    private int currentHitPoints;
+
+    private void Start() {
+        currentHitPoints = maxHitPoints;
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) {
+        Damage damage = other.gameObject.GetComponent<Damage>();
+        if (damage != null) {
+            currentHitPoints -= damage.hits;
+
+            if (currentHitPoints <= 0) {
+                Die();
+            }
+        }
+    }
+
+    private void Die() {
+        Debug.Log("you ded");
+    }
+}

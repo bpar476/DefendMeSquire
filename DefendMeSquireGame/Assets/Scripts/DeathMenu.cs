cd8b6b344905f10acb30898263f8ad631a1d0822
Ben Partridge <ben.partridge249@gmail.com>
2020-05-12 18:15:07 +1000

    Implement Death UI actions

---
+++ DefendMeSquireGame/Assets/Scripts/DeathMenu.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeathMenu : MonoBehaviour
+{
+    public void Restart() {
+        SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().name);
+    }
+
+    public void Quit() {
+        Application.Quit();
+    }
+}

03e518dc73af925c75cffa3c6a69ddaad7a80c7e
Ben Partridge <ben.partridge249@gmail.com>
2020-05-14 20:29:37 +1000

    Make arrow firing non-random and make it easier to set period. Also, arrows fire on t = 0

--- DefendMeSquireGame/Assets/Scripts/ArrowFirer.cs
+++ DefendMeSquireGame/Assets/Scripts/ArrowFirer.cs
@@ -5,47 +5,64 @@ using UnityEngine;
 public class ArrowFirer : MonoBehaviour
 {
 
-    public int shotsPerMinute;
+    public int period;
     public float timerOffset;
     public Vector2 trajectory;
     public float fireVelocity;
     public GameObject projectilePrefab;
     private Vector2 normalizedTrajectory;
-
     private float timer;
+    private bool firedFirstShot = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        timer = timerOffset;
+        timer = -timerOffset;
         normalizedTrajectory = trajectory.normalized;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (HasTimerTicked()) {
-            float rand = Random.Range(0.0f, 1.0f);
-            if (rand <= shotsPerMinute / 60.0f) {
-                fireProjectile();
-            }
+        if (HasTimerTicked())
+        {
+            // float rand = Random.Range(0.0f, 1.0f);
+            // if (rand <= shotsPerMinute / 60.0f) {
+            fireProjectile();
+            // }
         }
     }
 
-    private void fireProjectile() {
+    private void fireProjectile()
+    {
         GameObject projectile = Instantiate(projectilePrefab, transform.position, Quaternion.identity) as GameObject;
 
         Rigidbody2D projectileBody = projectile.GetComponent<Rigidbody2D>();
         projectileBody.velocity = fireVelocity * normalizedTrajectory;
     }
 
-    bool HasTimerTicked() {
+    bool HasTimerTicked()
+    {
         timer += Time.fixedDeltaTime;
-        if (timer > 1.0f) {
-            timer -= 1.0f;
+        if (!firedFirstShot && timer >= 0)
+        {
+            firedFirstShot = true;
+            return true;
+        }
+
+        if (timer > period)
+        {
+            timer -= period;
 
             return true;
         }
         return false;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.magenta;
+        Vector3 direction = trajectory.normalized;
+        Gizmos.DrawLine(transform.position, transform.position + direction);
     }
 }

12ecd944eb5f0c018f0dffd4f3beacfbb298a742
Ben Partridge <ben.partridge249@gmail.com>
2020-05-14 21:17:38 +1000

    Fix clunky collision on ladder toppers

--- DefendMeSquireGame/Assets/Scripts/LadderTopper.cs
+++ DefendMeSquireGame/Assets/Scripts/LadderTopper.cs
@@ -6,9 +6,11 @@ public class LadderTopper : MonoBehaviour
 {
     public Ladder ladder;
 
+    private bool effectorModified = false;
     private PlatformEffector2D effector2D;
 
-    private void Start() {
+    private void Start()
+    {
         effector2D = GetComponent<PlatformEffector2D>();
     }
 
@@ -17,12 +19,31 @@ public class LadderTopper : MonoBehaviour
     void Update()
     {
         GameObject player = GameObject.FindWithTag("Player");
+        if (player == null)
+        {
+            // Player is dead
+            gameObject.SetActive(false);
+            return;
+        }
+
         Vector3 playerPosition = player.transform.position;
-        if (playerPosition.y > transform.position.y + 0.5f && Input.GetAxis("Vertical") < 0) {
-            // Player is above tip of ladder
-            effector2D.rotationalOffset = 180;
-        } else {
-            effector2D.rotationalOffset = 0;
+        if (playerPosition.y > transform.position.y + 0.5f && Input.GetAxis("Vertical") < 0)
+        {
+
+            // Setting the rotation of the effector repeatedly messes with it's collision properties
+            if (!effectorModified)
+            {
+                effector2D.rotationalOffset = 180;
+                effectorModified = true;
+            }
+        }
+        else
+        {
+            if (effectorModified)
+            {
+                effector2D.rotationalOffset = 0;
+                effectorModified = false;
+            }
         }
     }
 }

eed965d4068082b8d4561c9ada22dff4ad964359
Ben Partridge <ben.partridge249@gmail.com>
2020-05-18 20:28:43 +1000

    Implement setting arrows active for duration of task

--- DefendMeSquireGame/Assets/Scripts/SquireTasks/SquireTaskSpawnPoint.cs
+++ DefendMeSquireGame/Assets/Scripts/SquireTasks/SquireTaskSpawnPoint.cs
@@ -2,16 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SquireTaskSpawnPoint : MonoBehaviour
+public class SquireTaskSpawnPoint : MonoBehaviour, SquireTaskCompletionListener
 {
     public Vector2 spawnLocation;
     public int spawnDelaySeconds;
     public SquireTask taskPrefab;
 
+    public List<GameObject> objectsActiveDuringThisTask;
+
+    public void onTaskCompleted()
+    {
+        objectsActiveDuringThisTask.ForEach(gobj => gobj.SetActive(false));
+    }
+
     public SquireTask Spawn()
     {
         SquireTask task = Instantiate(taskPrefab, transform.position, Quaternion.identity);
+        task.RegisterListener(this);
 
+        objectsActiveDuringThisTask.ForEach(gobj => gobj.SetActive(true));
         return task;
     }
 }

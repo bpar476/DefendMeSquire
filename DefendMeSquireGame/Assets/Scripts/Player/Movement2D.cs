8d4cc3fba811f446efb76a0bc5087a52b9a1e05b
Ben Partridge <ben.partridge249@gmail.com>
2020-05-21 18:03:33 +1000

    Use normal force mode for movement. Improves moving short distances.

--- DefendMeSquireGame/Assets/Scripts/Player/Movement2D.cs
+++ DefendMeSquireGame/Assets/Scripts/Player/Movement2D.cs
@@ -22,13 +22,16 @@ public class Movement2D : MonoBehaviour
         {
             rb2d.velocity = new Vector2(0, rb2d.velocity.y);
         } else {
+        }
+        else
+        {
             if (Input.GetAxis("Horizontal") != 0)
             {
                 float inputDir = Mathf.Sign(Input.GetAxis("Horizontal"));
 
                 if (inputDir * rb2d.velocity.x < maxMoveSpeed)
                 {
-                    rb2d.AddForce(new Vector2(inputDir * speed, 0), ForceMode2D.Impulse);
+                    rb2d.AddForce(new Vector2(inputDir * speed, 0));
                 }
 
                 rb2d.velocity = new Vector2(Mathf.Clamp(rb2d.velocity.x, -maxMoveSpeed, maxMoveSpeed), rb2d.velocity.y);

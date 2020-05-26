158b13a3275e8c324efcf031e88ffc067819569b
Ben Partridge <ben.partridge249@gmail.com>
2020-05-26 19:58:59 +1000

    Implement following the player vertically with the camera

---
+++ DefendMeSquireGame/Assets/Scripts/Scene/BoundedMovingCamera.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoundedMovingCamera : MonoBehaviour
+{
+
+    public float upperBound;
+    public float lowerBound;
+    private float cameraRadius = 0;
+    public Color gizmoColour;
+
+    private GameObject player;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = gizmoColour;
+        Vector3 direction = Vector3.up;
+        Gizmos.DrawLine(transform.position, transform.position - direction * cameraRadius);
+        Gizmos.DrawLine(transform.position, transform.position + direction * cameraRadius);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player == null)
+        {
+            this.enabled = false;
+        }
+        else
+        {
+            transform.position = new Vector3(transform.position.x, player.transform.position.y + 1.8f, transform.position.z);
+        }
+    }
+}

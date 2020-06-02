d6bc83e1cfac6fd1133f3821e9a1696104c1fb0f
Ben Partridge <ben.partridge249@gmail.com>
2020-06-02 19:43:19 +1000

    Implement generic, per-object timer script

---
+++ DefendMeSquireGame/Assets/Scripts/Misc/Timer.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Script for helping with timing of actions.
+/// Time is updated during FixedUpdate.
+/// </summary>
+public class Timer : MonoBehaviour
+{
+
+    public float period;
+    public float offset;
+
+    private float timer;
+
+
+    private bool running = false;
+    private bool tick = false;
+
+    private void Start()
+    {
+        timer = -offset;
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        if (running)
+        {
+            UpdateTimer();
+
+            UpdateTicked();
+        }
+    }
+
+    /// Starts this timer
+    public void StartTimer()
+    {
+        running = true;
+    }
+
+    /// Returns true if the timer has ticked this cycle, otherwise returns false
+    public bool Ticked()
+    {
+        return tick;
+    }
+
+    private void UpdateTimer()
+    {
+        timer += Time.fixedDeltaTime;
+    }
+
+    private void UpdateTicked()
+    {
+        if (timer > period)
+        {
+            timer -= period;
+
+            tick = true;
+        }
+        tick = false;
+    }
+
+}

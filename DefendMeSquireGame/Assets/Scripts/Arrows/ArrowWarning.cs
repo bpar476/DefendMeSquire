baf74318fdf0544b5c44361522c91912d4cb3e50
Ben Partridge <ben.partridge249@gmail.com>
2020-06-08 14:35:26 +1000

    Use rectangles in camera viewport calculation to make it easier to interact with

--- DefendMeSquireGame/Assets/Scripts/Arrows/ArrowWarning.cs
+++ DefendMeSquireGame/Assets/Scripts/Arrows/ArrowWarning.cs
@@ -109,24 +109,22 @@ public class ArrowWarning : MonoBehaviour, GlobalTimerStopwatch
     private (Vector2 intersection, bool found) GetWarningPosition()
     {
         var cam = Camera.main;
-        float width = cam.orthographicSize * cam.aspect;
-        // Decrease width by a bit so sign is in view
-        width -= 0.1f;
-        float height = cam.orthographicSize;
 
-        int side = transform.position.x < (cam.transform.position.x) - width ? -1 : 1;
+        Rect camRect = getCameraRect(cam);
+
+        int side = transform.position.x < (cam.transform.position.x) ? -1 : 1;
 
         Vector2 direction = firer.trajectory.normalized * 10;
         Vector2 projection = new Vector2(transform.position.x, transform.position.y) + direction;
 
-        float closestCameraEdgeX = cam.transform.position.x + side * width;
-        float cameraFloorY = cam.transform.position.y - height;
-        float cameraCeilingY = cameraFloorY + 2 * height;
-        Vector2 cameraEdgeBottom = new Vector2(closestCameraEdgeX, cameraFloorY);
-        Vector2 cameraEdgeHeight = new Vector2(closestCameraEdgeX, cameraCeilingY);
+        // Offset the camera sides a bit so the warning symbol is within the viewport
+        float cameraSideOffset = 0.1f;
+        float closestCameraEdgeX = transform.position.x < cam.transform.position.x ? camRect.xMin + cameraSideOffset : camRect.xMax - cameraSideOffset;
+        Vector2 closestCameraEdgeBottom = new Vector2(closestCameraEdgeX, camRect.yMin);
+        Vector2 closestCameraEdgeTop = new Vector2(closestCameraEdgeX, camRect.yMax);
 
         bool found;
-        Vector2 intersection = GetIntersectionPointCoordinates(transform.position, projection, cameraEdgeBottom, cameraEdgeHeight, out found);
+        Vector2 intersection = GetIntersectionPointCoordinates(transform.position, projection, closestCameraEdgeBottom, closestCameraEdgeTop, out found);
 
         if (found)
         {
@@ -134,15 +132,23 @@ public class ArrowWarning : MonoBehaviour, GlobalTimerStopwatch
         }
 
         // Maybe there is an intersection with the top of the viewport
-        float cameraLeftEdgeX = cam.transform.position.x - width;
-        float cameraRightEdgeX = cam.transform.position.x + width;
-        // Decrease height by a bit so sign is in view
-        cameraCeilingY -= 0.2f;
-        Vector2 cameraTopLeft = new Vector2(cameraLeftEdgeX, cameraCeilingY);
-        Vector2 cameraTopRight = new Vector2(cameraRightEdgeX, cameraCeilingY);
+        float cameraTopOffset = 0.2f;
+        Vector2 cameraTopLeft = new Vector2(camRect.xMin, camRect.yMax - cameraTopOffset);
+        Vector2 cameraTopRight = new Vector2(camRect.xMax, camRect.yMax - cameraTopOffset);
         intersection = GetIntersectionPointCoordinates(transform.position, projection, cameraTopLeft, cameraTopRight, out found);
 
         return (intersection, found);
+    }
+
+    private Rect getCameraRect(Camera cam)
+    {
+        float width = cam.orthographicSize * cam.aspect;
+        float height = cam.orthographicSize;
+
+        float cameraLeftEdgeX = cam.transform.position.x - width;
+        float cameraFloorY = cam.transform.position.y - height;
+
+        return new Rect(cameraLeftEdgeX, cameraFloorY, 2 * width, 2 * height);
     }
 
     private void OnDrawGizmosSelected()
@@ -158,6 +164,11 @@ public class ArrowWarning : MonoBehaviour, GlobalTimerStopwatch
         {
             Gizmos.DrawSphere(origin, 0.05f);
         }
+
+        var rect = getCameraRect(Camera.main);
+        // Offset the wirecube otherwise it overlaps with the camera viewport gizmo
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireCube(rect.center, rect.size - new Vector2(0.1f, 0.1f));
     }
 
     // Snippet and theory from: https://blog.dakwamine.fr/?p=1943

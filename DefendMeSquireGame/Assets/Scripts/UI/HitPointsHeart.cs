af2d56a17ab3cbeaa48d8217f8ff913cd4028425
Josh Cleland <jcle919@aucklanduni.ac.nz>
2020-06-22 22:34:03 +1200

    Add hit points heart script

---
+++ DefendMeSquireGame/Assets/Scripts/UI/HitPointsHeart.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HitPointsHeart : MonoBehaviour
+{
+    public Sprite HealthyHeart;
+    public Sprite BrokenHeart;
+    private Image heart;
+    // Start is called before the first frame update
+    void Start()
+    {
+        heart = GetComponent<Image>();
+        heart.sprite = HealthyHeart;
+    }
+}

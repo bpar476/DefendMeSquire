5fd29155939202453de558735b9a6a2d53e4b42f
Ben Partridge <ben.partridge249@gmail.com>
2020-06-25 20:28:59 +1000

    Make health scripts run in Edit mode so it's easier to visualise level

--- DefendMeSquireGame/Assets/Scripts/Player/Health.cs
+++ DefendMeSquireGame/Assets/Scripts/Player/Health.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+[ExecuteInEditMode]
 public class Health : MonoBehaviour, Killable
 {
     public int maxHitPoints;
--- DefendMeSquireGame/Assets/Scripts/UI/HitPointsUI.cs
+++ DefendMeSquireGame/Assets/Scripts/UI/HitPointsUI.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+[ExecuteInEditMode]
 public class HitPointsUI : MonoBehaviour
 {
     public HitPointsSymbol hitPointsSymbol;

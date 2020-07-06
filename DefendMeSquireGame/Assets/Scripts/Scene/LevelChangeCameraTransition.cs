2c6c67879391009843dbc521af94d6f1f66f5397
Ben Partridge <ben.partridge249@gmail.com>
2020-07-06 20:20:08 +1000

    Implement camera bounds updating when player moves up level

---
+++ DefendMeSquireGame/Assets/Scripts/Scene/LevelChangeCameraTransition.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelChangeCameraTransition : MonoBehaviour
+{
+    public float newUpperBound;
+    public float newLowerBound;
+
+    public void UpdateCameraBounds()
+    {
+        var cam = Camera.main.GetComponent<BoundedMovingCamera>();
+        cam.upperBound = newUpperBound;
+        cam.lowerBound = newLowerBound;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            UpdateCameraBounds();
+        }
+    }
+}

d1b8e7e24162d57f19951f8674dab48ebe6101b7
Ben Partridge <ben.partridge249@gmail.com>
2020-08-12 17:51:53 +1000

    Implement main menu logic

---
+++ DefendMeSquireGame/Assets/Scripts/SceneManagement/DefendMeSquireScenes.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum DefendMeSquireScenes
+{
+    MainMenu = 0,
+    FirstLevel = 1,
+    PrototypeLevel = 2
+}
---
+++ DefendMeSquireGame/Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    private bool hasLoadedLevel = false;
+
+    public void StartGame()
+    {
+        if (!hasLoadedLevel)
+        {
+            SceneManager.LoadSceneAsync((int)DefendMeSquireScenes.FirstLevel);
+            hasLoadedLevel = true;
+        }
+    }
+
+    public void QuitToDesktop()
+    {
+        Application.Quit();
+    }
+}

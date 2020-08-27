32c1551882e10bb8dc165a0051293d5d189013fa
Ben Partridge <ben.partridge249@gmail.com>
2020-08-28 08:12:45 +1000

    Implement lowering  drawbridge with crank valve

---
+++ DefendMeSquireGame/Assets/Scripts/Drawbridge/Drawbridge.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drawbridge : AbstractTriggeredEvent
+{
+    public override void OnTrigger()
+    {
+        Debug.Log("dropping drawbridge");
+        StartCoroutine(LowerDrawbridge());
+    }
+
+    private IEnumerator LowerDrawbridge()
+    {
+        for (float i = 90; i >= 0; i -= 30 * Time.deltaTime)
+        {
+            transform.rotation = Quaternion.Euler(0, 0, i);
+            yield return null;
+        }
+        transform.rotation = Quaternion.Euler(0, 0, 0);
+        yield return null;
+    }
+}

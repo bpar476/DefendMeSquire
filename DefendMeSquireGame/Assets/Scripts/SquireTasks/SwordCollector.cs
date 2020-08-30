4d4e88ffb967e7c0125b1ddf8e5e5b9daa5ae972
Ben Partridge <ben.partridge249@gmail.com>
2020-08-31 08:57:12 +1000

    Implement sword task drop off point

---
+++ DefendMeSquireGame/Assets/Scripts/SquireTasks/SwordCollector.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordCollector : MonoBehaviour
+{
+    public SquireTaskCompletionListener[] completionListeners;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        CollectSword(other.GetComponent<SwordTask>());
+    }
+
+    private void CollectSword(SwordTask sword)
+    {
+        if (sword == null)
+        {
+            throw new System.Exception("No sword component in SwordCollector receiver. Layer matrix is incorrect");
+        }
+        sword.GiveSword(transform);
+        foreach (var listener in completionListeners)
+        {
+            listener.onTaskCompleted();
+        }
+    }
+}
--- DefendMeSquireGame/Assets/Scripts/SquireTasks/SwordTask.cs
+++ DefendMeSquireGame/Assets/Scripts/SquireTasks/SwordTask.cs
@@ -7,6 +7,7 @@ public class SwordTask : MonoBehaviour
     private bool isPlayerOnSword = false;
     private GameObject player = null;
     private bool isPlayerHoldingSword = false;
+    private float originalPlayerMoveSpeed;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
@@ -22,18 +23,25 @@ public class SwordTask : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (!isPlayerHoldingSword && Input.GetKeyDown(KeyCode.E))
+        if (!isPlayerHoldingSword && isPlayerOnSword && Input.GetKeyDown(KeyCode.E))
         {
             PickUpSword();
         }
     }
 
-
-
     private void PickUpSword()
     {
         isPlayerHoldingSword = true;
         transform.SetParent(player.transform);
-        player.GetComponent<Movement2D>().maxMoveSpeed = 1;
+
+        Movement2D playerMovement = player.GetComponent<Movement2D>();
+        originalPlayerMoveSpeed = playerMovement.maxMoveSpeed;
+        playerMovement.maxMoveSpeed = 1;
+    }
+
+    public void GiveSword(Transform receiver)
+    {
+        transform.SetParent(receiver);
+        player.GetComponent<Movement2D>().maxMoveSpeed = originalPlayerMoveSpeed;
     }
 }

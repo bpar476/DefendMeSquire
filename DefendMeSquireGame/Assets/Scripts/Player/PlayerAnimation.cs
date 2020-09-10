df1f484cf7c17366fbdbf6d1ba1dc69d04f85367
Ben Partridge <ben.partridge249@gmail.com>
2020-09-11 07:26:51 +1000

    Fix bug where climbing animation would play when not actually moving due to climbing against a collider

--- DefendMeSquireGame/Assets/Scripts/Player/PlayerAnimation.cs
+++ DefendMeSquireGame/Assets/Scripts/Player/PlayerAnimation.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(Animator), typeof(Rigidbody2D))]
 public class PlayerAnimation : MonoBehaviour, MovementListener, DeathListener
 {
 
@@ -12,16 +12,31 @@ public class PlayerAnimation : MonoBehaviour, MovementListener, DeathListener
     private static readonly string BOOL_ON_LADDER = "onLadder";
 
     private Animator animator;
+    private Rigidbody2D rb2d;
     private bool turnedAround = false;
     private Vector3 originalLocalScale;
     private Vector3 turnedAroundLocalScale;
+    private bool tryingToClimbLadder = false;
 
     private void Awake()
     {
         animator = GetComponent<Animator>();
+        rb2d = GetComponent<Rigidbody2D>();
         var localScale = transform.localScale;
         originalLocalScale = new Vector3(localScale.x, localScale.y, localScale.z);
         turnedAroundLocalScale = new Vector3(-localScale.x, localScale.y, localScale.z);
+    }
+
+    private void FixedUpdate()
+    {
+        if (Mathf.Abs(rb2d.velocity.y) > 0.1 && tryingToClimbLadder)
+        {
+            animator.SetBool(BOOL_CLIMBING, true);
+        }
+        else
+        {
+            animator.SetBool(BOOL_CLIMBING, false);
+        }
     }
 
     public void OnDeath()
@@ -51,12 +66,12 @@ public class PlayerAnimation : MonoBehaviour, MovementListener, DeathListener
 
     public void OnLadderClimb()
     {
-        animator.SetBool(BOOL_CLIMBING, true);
+        tryingToClimbLadder = true;
     }
 
     public void OnLadderHalt()
     {
-        animator.SetBool(BOOL_CLIMBING, false);
+        tryingToClimbLadder = false;
     }
 
     public void OnLadderDismount()

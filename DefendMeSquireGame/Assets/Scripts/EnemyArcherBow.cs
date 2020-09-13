7b76279a2168198523e8dc1e407e081d5bb0f8ff
Ben Partridge <ben.partridge249@gmail.com>
2020-09-14 08:39:58 +1000

    Add archer enemy which points its bow at the player

---
+++ DefendMeSquireGame/Assets/Scripts/EnemyArcherBow.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyArcherBow : MonoBehaviour
+{
+    [SerializeField]
+    private Transform bow;
+
+    private float originalXScale;
+
+    private void Awake()
+    {
+        originalXScale = transform.localScale.x;
+    }
+
+    private void FixedUpdate()
+    {
+        var playerPosition = GameObject.FindGameObjectWithTag("Player").transform.position;
+        float direction = 1.0f;
+
+        if (playerPosition.x > transform.position.x)
+        {
+            direction = -1.0f;
+        }
+
+        transform.localScale = new Vector3(direction * originalXScale, transform.localScale.y, transform.localScale.z);
+
+        var lineFromBowToPlayer = playerPosition - bow.transform.position;
+        var angleBetweenBowAndPlayer = Vector2.Angle(direction * Vector2.left, lineFromBowToPlayer);
+        bow.localEulerAngles = new Vector3(0, 0, angleBetweenBowAndPlayer);
+    }
+}

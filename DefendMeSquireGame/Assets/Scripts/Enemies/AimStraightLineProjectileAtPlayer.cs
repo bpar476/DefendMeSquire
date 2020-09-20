c942fe66072f6021b607f8192e016636b555fa0e
Ben Partridge <ben.partridge249@gmail.com>
2020-09-21 07:02:52 +1000

    Add script for aiming a projectile in a straight line at the player

---
+++ DefendMeSquireGame/Assets/Scripts/Enemies/AimStraightLineProjectileAtPlayer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(ProjectileFirer))]
+public class AimStraightLineProjectileAtPlayer : MonoBehaviour
+{
+
+    private ProjectileFirer firer;
+
+    private void Awake()
+    {
+        firer = GetComponent<ProjectileFirer>();
+        AimAtPlayer();
+    }
+
+    private void Update()
+    {
+        AimAtPlayer();
+    }
+
+    private void AimAtPlayer()
+    {
+        var player = GameObject.FindGameObjectWithTag("Player");
+
+        if (player == null)
+        {
+            Debug.Log("player is null. Disabling enemy aiming");
+            this.enabled = false;
+        }
+        else
+        {
+            firer.trajectory = player.transform.position - transform.position;
+        }
+    }
+}

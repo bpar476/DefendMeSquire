6cb3f12a6430dab7ba4cb13c0026f911dbf72dde
Ben Partridge <ben.partridge249@gmail.com>
2020-09-22 08:47:14 +1000

    Rework fireball sprite and animation

---
+++ DefendMeSquireGame/Assets/FireballDisappear.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireballDisappear : StateMachineBehaviour
+{
+
+    private Fireball fireball;
+
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo animatorStateInfo, int layerIndex)
+    {
+        fireball = animator.gameObject.GetComponent<Fireball>();
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        fireball.OnExplosionFinished();
+    }
+}
--- DefendMeSquireGame/Assets/Scripts/Enemies/Fireball.cs
+++ DefendMeSquireGame/Assets/Scripts/Enemies/Fireball.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(AudioSource), typeof(SpriteRenderer), typeof(Collider2D))]
-[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Rigidbody2D), typeof(Animator))]
 public class Fireball : Projectile
 {
+    private static readonly string ANIM_TRIGGER_EXPLODE = "Impact";
+
     [SerializeField]
     private AudioClip castClip;
     [SerializeField]
@@ -19,6 +21,8 @@ public class Fireball : Projectile
     private new Collider2D collider;
     private Rigidbody2D rb2d;
     private bool hasDetonated;
+    private float rotation = 0;
+    private Animator animator;
 
     private void Awake()
     {
@@ -26,6 +30,7 @@ public class Fireball : Projectile
         spriteRenderer = GetComponent<SpriteRenderer>();
         collider = GetComponent<Collider2D>();
         rb2d = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     private void Start()
@@ -45,6 +50,8 @@ public class Fireball : Projectile
                 DetonateFireball();
             }
         }
+        rotation += 2f;
+        transform.localEulerAngles = new Vector3(0, 0, -(rotation % 360));
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -59,13 +66,20 @@ public class Fireball : Projectile
     {
         if (!hasDetonated)
         {
+            rb2d.velocity = Vector2.zero;
             hasDetonated = true;
+            animator.SetTrigger(ANIM_TRIGGER_EXPLODE);
             audioSource.clip = impactClip;
             audioSource.Play();
-            spriteRenderer.enabled = false;
             collider.enabled = false;
-            StartCoroutine(DestroyFireBallAfterClipFinished());
         }
+    }
+
+    // Called by fireball StateMachineBehaviour
+    public void OnExplosionFinished()
+    {
+        spriteRenderer.enabled = false;
+        StartCoroutine(DestroyFireBallAfterClipFinished());
     }
 
     private IEnumerator DestroyFireBallAfterClipFinished()
@@ -75,6 +89,6 @@ public class Fireball : Projectile
         {
             yield return new WaitForSeconds(0.1f);
         }
-        Destroy(this.gameObject);
+        GameObject.Destroy(this.gameObject);
     }
 }

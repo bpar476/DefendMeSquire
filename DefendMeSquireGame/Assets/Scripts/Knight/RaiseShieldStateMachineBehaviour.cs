e2e996dd1e4a48352a6bb5a4ece1f8b190f1cd7f
Ben Partridge <ben.partridge249@gmail.com>
2020-09-29 08:22:39 +1000

    Implement knight moving to defend squire while squire turns drawbridge crank

---
+++ DefendMeSquireGame/Assets/Scripts/Knight/RaiseShieldStateMachineBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaiseShieldStateMachineBehaviour : MyStateMachineBehaviour
+{
+    [SerializeField]
+    private GameObject shield;
+
+    public override void OnStateEnter()
+    {
+        shield.SetActive(true);
+    }
+
+    public override bool OnStateUpdate()
+    {
+        // Listen for when crank has been fully turned
+        return false;
+    }
+}

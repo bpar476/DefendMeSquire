77a10eef0dfb32de847ea6199ce71d21a62c8444
Ben Partridge <ben.partridge249@gmail.com>
2020-10-17 14:07:46 +1100

    Implement knight with shield and shield animation

--- DefendMeSquireGame/Assets/Scripts/Knight/RaiseShieldStateMachineBehaviour.cs
+++ DefendMeSquireGame/Assets/Scripts/Knight/RaiseShieldStateMachineBehaviour.cs
@@ -4,17 +4,23 @@ using UnityEngine;
 
 public class RaiseShieldStateMachineBehaviour : MyStateMachineBehaviour
 {
+    private readonly string ANIM_TRIGGER_SHIELD = "shield";
+
     [SerializeField]
     private GameObject shield;
 
     [SerializeField]
     private AbstractTrigger trigger;
 
+    private Animator animator;
+
     // TODO refactor this to use the TriggeredEventStateMachineBehaviour
     private bool hasFinished;
 
     private void Awake()
     {
+        animator = GetComponent<Animator>();
+
         trigger.OnTrigger += () =>
         {
             hasFinished = true;
@@ -24,10 +30,15 @@ public class RaiseShieldStateMachineBehaviour : MyStateMachineBehaviour
     public override void OnStateEnter()
     {
         shield.SetActive(true);
+        animator.SetTrigger(ANIM_TRIGGER_SHIELD);
     }
 
     public override bool OnStateUpdate()
     {
+        if (hasFinished)
+        {
+            shield.SetActive(false);
+        }
         return hasFinished;
     }
 }

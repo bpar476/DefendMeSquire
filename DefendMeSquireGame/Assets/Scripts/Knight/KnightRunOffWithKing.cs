baba5b638124dc66c22338ce067905e66c51559e
Ben Partridge <ben.partridge249@gmail.com>
2020-10-23 08:36:34 +1100

    Implement end game story flowchart

---
+++ DefendMeSquireGame/Assets/Scripts/Knight/KnightRunOffWithKing.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KnightRunOffWithKing : MonoBehaviour
+{
+
+    [SerializeField]
+    public Transform king;
+
+    public void FleeToVictory()
+    {
+        king.transform.SetParent(transform);
+        king.transform.localPosition = new Vector3(0.6f, -0.4f, 0);
+        StartCoroutine(JumpToTheLeft());
+    }
+
+    private IEnumerator JumpToTheLeft()
+    {
+        for (var i = 0; i < (5 * 100); i++)
+        {
+            transform.position += new Vector3(-0.15f, 0.1f, 0);
+            yield return new WaitForFixedUpdate();
+        }
+
+        yield return null;
+    }
+}

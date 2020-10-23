f584362a87cc1c0d1d92a4056b4a8a56e3d98842
Ben Partridge <ben.partridge249@gmail.com>
2020-10-23 18:10:55 +1100

    Implement starting the end game cutscene after the knight fights the Wizard

---
+++ DefendMeSquireGame/Assets/Scripts/Util/TriggerFlowchartStateMachineBehaviour.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+
+public class TriggerFlowchartStateMachineBehaviour : MyStateMachineBehaviour
+{
+
+    /// <summary>
+    /// The fungus flowchart containing the block to execute
+    /// </summary>
+    [SerializeField]
+    private Flowchart flowchartToTrigger;
+
+    /// <summary>
+    /// The name of the block on the flowchart to execute
+    /// </summary>
+    [SerializeField]
+    private string flowchartBlock;
+
+    private bool hasTriggered;
+
+    public override void OnStateEnter()
+    {
+        Debug.Log("triggering block");
+        hasTriggered = true;
+        flowchartToTrigger.ExecuteBlock("End of game narrative");
+    }
+
+    public override bool OnStateUpdate()
+    {
+        return hasTriggered && !flowchartToTrigger.HasExecutingBlocks();
+    }
+}

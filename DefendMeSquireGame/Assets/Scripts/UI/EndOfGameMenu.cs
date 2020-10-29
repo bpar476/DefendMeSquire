df0eec95dc06247ecb4bd2bbd349a0074b18206f
Ben Partridge <ben.partridge249@gmail.com>
2020-10-30 09:11:28 +1100

    Implement end of game scene

---
+++ DefendMeSquireGame/Assets/Scripts/UI/EndOfGameMenu.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndOfGameMenu : MonoBehaviour
+{
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene((int)DefendMeSquireScenes.MainMenu);
+    }
+}
---
+++ DefendMeSquireGame/Assets/Scripts/UI/LoadWebPage.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadWebPage : MonoBehaviour
+{
+    [SerializeField]
+    private string URL;
+
+    public void OpenWebPage()
+    {
+        Application.OpenURL(URL);
+    }
+}
